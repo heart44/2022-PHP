6ea7a050fdff69015fa4cb2608784c1cd7c54602
heart44 <gutt88656@gmail.com>
2022-05-24 16:52:16 +0900

    bootstrap 컨텐츠, 폼(1)

---
+++ bootstrap/figure.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <figure>
+            <img src="https://i.picsum.photos/id/165/500/300.jpg?hmac=KqV50FV4IOuVeKq7VdYC9URDbATDribibueuR6jbjqw" class="figure-img img-fluid rounded" alt="...">
+            <figcaption>A caption for the above image.</figcaption>
+        </figure>
+    </div>
+</body>
+
+</html>
---
+++ bootstrap/img.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <title>부트스트랩</title>
+</head>
+
+<body>
+    <div class="container">
+        <img src="https://img.sbs.co.kr/newimg/news/20211029/201605033.jpg" class="img-fluid" alt="이미지">
+        <hr>
+        <img src="https://cdn.pixabay.com/photo/2013/07/13/01/12/witch-155291__340.png" class="img-thumbnail" alt="이미지">
+        <hr>
+        <div class="clearfix">
+            <img src="https://cdn.pixabay.com/photo/2013/07/13/01/12/witch-155291__340.png" class="rounded float-start" alt="이미지">
+            <img src="https://cdn.pixabay.com/photo/2013/07/13/01/12/witch-155291__340.png" class="rounded float-end" alt="이미지">
+        </div>
+        <hr>
+        <!-- 중앙정렬 -->
+        <img src="https://cdn.pixabay.com/photo/2013/07/13/01/12/witch-155291__340.png" class="rounded mx-auto d-block" alt="">
+        <hr>
+        <div class="d-flex justify-content-center">
+            <img src="https://cdn.pixabay.com/photo/2013/07/13/01/12/witch-155291__340.png" class="rounded" alt="이미지">
+        </div>
+        <hr>
+        <div style="text-align: center;">
+            <img src="https://cdn.pixabay.com/photo/2013/07/13/01/12/witch-155291__340.png" class="rounded" alt="이미지">
+        </div>
+        <hr>
+        <div class="text-center">
+            <img src="https://cdn.pixabay.com/photo/2013/07/13/01/12/witch-155291__340.png" class="rounded" alt="이미지">
+        </div>
+        <!-- 중앙정렬 끝 -->
+    </div>
+</body>
+
+</html>
