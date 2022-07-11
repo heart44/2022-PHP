d0c23c87ae81cfa7be3509a21a47e8d3b108492d
heart44 <55880047+heart44@users.noreply.github.com>
2022-07-11 17:14:12 +0900

    Update db.php
    database 수정하기

--- board_project/database/db.php
+++ board_project/database/db.php
@@ -2,7 +2,7 @@
 define("URL", "localhost");
 define("USERNAME", "root");
 define("PASSWORD", "1234");     // heidisql (506greendg@)
-define("DB_NAME", "board_project");
+define("DB_NAME", "board_project"); //heidisql (board_login)
 
 function get_conn() {
     return mysqli_connect(URL, USERNAME, PASSWORD, DB_NAME);
