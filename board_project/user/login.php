f0fc9e6b8da561c673a3b5023fe3c81aced9ebb5
heart44 <55880047+heart44@users.noreply.github.com>
2022-07-11 17:20:17 +0900

    수정(1)

--- board_project/user/login.php
+++ board_project/user/login.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="css/container.css">
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/container.css">
     <title>로그인</title>
 </head>
 <body>
     <div id="container">
-        <?php include('header.php') ?>
+        <?php include('../header.php') ?>
         <div class="main">
             <h1>로그인</h1>
             <div class="login_area">
